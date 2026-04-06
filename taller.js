05bac047eb4079881edb079963691b53920b4aad
Mariana-121 <mlealr@unbosque.edu.co>
2026-04-06 08:54:51 -0500

    Create taller.js

---
+++ taller.js
@@ -0,0 +1,11 @@
+//Datos del usuario 
+let nombre = prompt("Ingresa tu nombre completo:");
+let edad = parseInt(prompt("Ingresa tu edad:"));
+let tipodedocumento = prompt("Ingresa tu tipo de documento (CC, TI, CE, etc):");
+let numerodedocumento = prompt("Ingresa tu número de documento:");
+
+//Mostrar info
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Tipo de documento: " + tipodedocumento);
+console.log("Número de documento: " + numerodedocumento);
