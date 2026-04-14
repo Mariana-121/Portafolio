59dcd6a6157ad6e41254acd0c122c5399a5af586
Mariana-121 <mlealr@unbosque.edu.co>
2026-04-14 07:36:06 -0500

    Update taller.js

--- taller.js
+++ taller.js
@@ -1,11 +1,28 @@
 //Datos del usuario 
+
 let nombre = prompt("Ingresa tu nombre completo:");
 let edad = parseInt(prompt("Ingresa tu edad:"));
-let tipodedocumento = prompt("Ingresa tu tipo de documento (CC, TI, CE, etc):");
+let tipoDeDocumento = prompt("Ingresa tu tipo de documento (CC, TI, CE, etc):");
 let numerodedocumento = prompt("Ingresa tu número de documento:");
 
 //Mostrar info
+
 console.log("Nombre: " + nombre);
 console.log("Edad: " + edad);
-console.log("Tipo de documento: " + tipodedocumento);
-console.log("Número de documento: " + numerodedocumento);
+console.log("Tipo de documento: " + tipoDeDocumento);
+console.log("Número de documento: " + numeroDeDocumento);
+
+// Validación de la edad
+
+if (edad < 18) {
+    console.log("El usuario es menor de edad, no puede continuar");
+} 
+else if (edad < 25) {
+    console.log("El usuario es beneficiario por cotizante, no continúa");
+} 
+else if (edad >= 60) {
+    console.log("El usuario solo aplica para pensión");
+} 
+else {
+    console.log("El usuario puede continuar con el proceso");
+}
